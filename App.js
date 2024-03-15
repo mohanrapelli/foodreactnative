670d0a0798abf1c2aeb221066414bbedf457f06d
mohanrapelli <mohansomerholic198@gmail.com>
2024-03-15 16:37:51 +0530

    first commit

--- App.js
+++ App.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignIn from './Screens/SignIn';
-import Forgotpage from './Screens/Forgotpage';
-import Home from './Screens/Home';
-import Search from './Screens/Search';
-import Order from './Screens/Order';
+import Welcome from './Screens/Welcome';
+import Register from './Screens/Register';
+import Bottom from './Screens/Bottom';
+import Coin from './Screens/Coin';
+import Resetpass from './Screens/Resetpass';
+import Referal from './Screens/Referal';
+import Shop from './Screens/Shop';
 import Profile from './Screens/Profile';
-import Homee from './Screens/Homee';
-import Edit from './Screens/Edit';
-import Payment from './Screens/Payment';
-import Fav from './Screens/Fav';
-import Reset from './Screens/Reset';
-import Changepass from './Screens/Changepass';
-import Phoneone from './Screens/Phoneone';
-import Phonetwo from './Screens/Phonetwo';
-import Adress from './Screens/Adress';
-
+import Googleplay from './Screens/Googleplay';
+import Email from './Screens/Email';
+import Flipcart from './Screens/Flipcart';
+import Upcoming from './Screens/Upcoming';
+import Live from './Screens/Live';
+import Earncoins from './Screens/Earncoins';
+import Confrimpass from './Screens/Confrimpass';
+import Login from './Screens/Login';
+import Home from './Screens/Home';
+import Settings from './Screens/Settings';
+import Language from './Screens/Language';
+import Usdt from './Screens/Usdt';
+import Watch from './Screens/Watch';
+import Color from './Screens/Color';
+import Redeem from './Screens/Redeem';
+import State from './Screens/State';
+import Country from './Screens/Country';
+import Help from './Screens/Help';
+import Chatroom from './Screens/Chatroom';
+import Paymentmethod from './Screens/Paymentmethod';
+import Contactus from './Screens/Contactus';
+import Headset from './Screens/Headset';
+import Phone from './Screens/Phone';
+import Placeorder from './Screens/Placeorder';
+import Orderhistory from './Screens/Orderhistory';
+import Rewards from './Screens/Rewards';
+import Freefire from './Screens/Freefire';
+import Upicash from './Screens/Upicash';
+import Verifyphone from './Screens/Verifyphone';
+import Otp from './Screens/Otp';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,19 +46,174 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer
-  >
-      <Stack.Navigator initialRouteName="SignIn"
-      
-      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Navigator initialRouteName="Welcome"
+
+        screenOptions={{ headerShown: false }}
       >
         <Stack.Screen
-          name="SignIn"
-          component={SignIn}
+          name="Welcome"
+          component={Welcome}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="Forgotpage"
-          component={Forgotpage}
+          name="Otp"
+          component={Otp}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen
+          name="Verifyphone"
+          component={Verifyphone}
+          options={{ headerShown: false }}
+        />
+          <Stack.Screen
+          name="Confrimpass"
+          component={Confrimpass}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Resetpass"
+          component={Resetpass}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen
+          name="Upicash"
+          component={Upicash}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Email"
+          component={Email}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen
+          name="Freefire"
+          component={Freefire}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen
+          name="Flipcart"
+          component={Flipcart}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen
+          name="Googleplay"
+          component={Googleplay}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Usdt"
+          component={Usdt}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Redeem"
+          component={Redeem}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Rewards"
+          component={Rewards}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Orderhistory"
+          component={Orderhistory}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Placeorder"
+          component={Placeorder}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Phone"
+          component={Phone}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Headset"
+          component={Headset}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Chatroom"
+          component={Chatroom}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Watch"
+          component={Watch}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Paymentmethod"
+          component={Paymentmethod}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Contactus"
+          component={Contactus}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Help"
+          component={Help}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Country"
+          component={Country}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Color"
+          component={Color}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="State"
+          component={State}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Language"
+          component={Language}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={Settings}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Earncoins"
+          component={Earncoins}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Live"
+          component={Live}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Upcoming"
+          component={Upcoming}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Bottom"
+          component={Bottom}
           options={{ headerShown: false }}
         />
         <Stack.Screen
@@ -44,67 +221,27 @@ export default function App() {
           component={Home}
           options={{ headerShown: false }}
         />
-         <Stack.Screen
-          name="Homee"
-          component={Homee}
+        <Stack.Screen
+          name="Coin"
+          component={Coin}
           options={{ headerShown: false }}
         />
-         <Stack.Screen
-          name="Search"
-          component={Search}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen
-          name="Order"
-          component={Order}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen
+        <Stack.Screen
           name="Profile"
           component={Profile}
           options={{ headerShown: false }}
         />
-         <Stack.Screen
-          name="Payment"
-          component={Payment}
+        <Stack.Screen
+          name="Shop"
+          component={Shop}
           options={{ headerShown: false }}
         />
-         <Stack.Screen
-          name="Edit"
-          component={Edit}
+        <Stack.Screen
+          name="Referal"
+          component={Referal}
           options={{ headerShown: false }}
         />
-         <Stack.Screen
-          name="Adress"
-          component={Adress}
-          options={{ headerShown: false }}
-        />
-        
-         <Stack.Screen
-          name="Fav"
-          component={Fav}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen
-          name="Phoneone"
-          component={Phoneone}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen
-          name="Phonetwo"
-          component={Phonetwo}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen
-          name="Reset"
-          component={Reset}
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen
-          name="Changepass"
-          component={Changepass}
-          options={{ headerShown: false }}
-        />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,65 +1,247 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import Profile from './Profile'; 
-import Search from './Search';
-import Homee from './Homee';
-import Order from './Order';
-import { Ionicons } from '@expo/vector-icons'; 
-import { FontAwesome5 } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-
-const Tab = createBottomTabNavigator();
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Image,ImageBackground, TouchableOpacity,onPress, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 export default function Home() {
+    const navigation = useNavigation();
   return (
-    
-      <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: 'red',
-        }}
-      >
-        <Tab.Screen
-          name="Homee"
-          component={Homee}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home" color={color} size={size} />
-            ),
-            headerShown: false, 
-          }}
-        />
-        <Tab.Screen
-          name="Search"
-          component={Search}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="search" color={color} size={size} />
-            ),
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="Order"
-          component={Order}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="cart" color={color} size={size} />
-            ),
-            headerShown: false, 
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="account-circle" size={size} color={color} />
-            ),
-            headerShown: false, 
-          }}
-        />
-      </Tab.Navigator>
+    <ScrollView>
+    <View style={styles.container}>
+      <View style={{ flexDirection: 'row',marginTop:-15 }}>
+        <Text style={styles.text}>S</Text>
+        <Text style={styles.text1}>UPREME</Text>
+ 
+        <Image
+     
+      source={require('../assets/Group.png')}
+        style={styles.image}/>
+      </View>
+      <View style={{ backgroundColor: 'purple', width: 400, height: 60 , flexDirection:'row',marginTop:14,}}>
+      <Text style={styles.text2}>
+        Winner
+      </Text>
+      <Text
+       onPress={()=> navigation.navigate("Live")}
+      style={styles.text3}>
+        Live
+      </Text>
+      <Text
+      onPress={()=> navigation.navigate("Upcoming")}
+      style={styles.text4}>
+        Upcoming
+      </Text>
+    </View>
    
+   
+    <ImageBackground source={require('../assets/bgm.png')} style={styles.background}>
+     
+      <View style={{ flexDirection: 'row' }}>
+       
+        <Image source={require('../assets/menimage.png')}
+        style={styles.image1}/>
+        <View>
+        <Text style={styles.text5}>1.Namer</Text>
+        <Text style={styles.text6}>
+  Ticket id: #2345678
+  </Text>
+  </View>
+  <View >
+    <Text style={styles.text7}>Product Won</Text>
+    <Image source={require('../assets/phone.png')}
+        style={styles.image2}/>
+       
+  </View>
+  <Text style={styles.text8}>The phone comes with a 3.50-inch display offering a resolution of 320x480 pixels. Apple iPhone is powered by a 412 MHz one-core processor. It comes with 128MB of RAM.</Text>
+ 
+ 
+   
+ 
+</View>
+ 
+ 
+ 
+</ImageBackground>
+<ImageBackground source={require('../assets/bgm.png')} style={styles.background}>
+     
+      <View style={{ flexDirection: 'row' }}>
+       
+        <Image source={require('../assets/menimage.png')}
+        style={styles.image1}/>
+        <View>
+        <Text style={styles.text5}>2.Ashok</Text>
+        <Text style={styles.text6}>
+  Ticket id: #2345678
+  </Text>
+  </View>
+  <View >
+    <Text style={styles.text7}>Product Won</Text>
+    <Image source={require('../assets/headphone.png')}
+        style={styles.image2}/>
+       
+  </View>
+  <Text style={styles.text8}>The phone comes with a 3.50-inch display offering a resolution of 320x480 pixels. Apple iPhone is powered by a 412 MHz one-core processor. It comes with 128MB of RAM.</Text>
+ 
+ 
+   
+ 
+</View>
+ 
+ 
+ 
+</ImageBackground>
+<ImageBackground source={require('../assets/bgm.png')} style={styles.background}>
+     
+      <View style={{ flexDirection: 'row' }}>
+       
+        <Image source={require('../assets/menimage.png')}
+        style={styles.image1}/>
+        <View>
+        <Text style={styles.text5}>3.Krishna</Text>
+        <Text style={styles.text6}>
+  Ticket id: #2345678
+  </Text>
+  </View>
+  <View >
+    <Text style={styles.text7}>Product Won</Text>
+    <Image source={require('../assets/watch.png')}
+        style={styles.image2}/>
+       
+  </View>
+  <Text style={styles.text8}>The phone comes with a 3.50-inch display offering a resolution of 320x480 pixels. Apple iPhone is powered by a 412 MHz one-core processor. It comes with 128MB of RAM.</Text>
+ 
+ 
+   
+ 
+</View>
+
+ 
+ 
+ 
+</ImageBackground>
+   
+ 
+<View style={{height:30}}>
+
+</View>
+ 
+    </View>
+    </ScrollView>
+   
+   
+ 
   );
 }
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    
+ 
+ 
+ 
+ 
+ 
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "orange",
+    marginTop:40,
+  },
+  text1: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "purple",
+    marginTop:40,
+  },
+  image: {
+    marginTop:40,
+    marginLeft:150,
+ 
+ 
+},
+text2: {
+  fontSize: 18,
+  fontWeight: "bold",
+  color: "white",
+  color:"green",
+  marginLeft:50,
+ marginTop:15,
+ 
+ 
+},
+text3: {
+  fontSize: 18,
+  fontWeight: "bold",
+  color: "white",
+  marginLeft:80,
+ marginTop:15,
+ 
+},
+text4: {
+  fontSize: 18,
+  fontWeight: "bold",
+  color: "white",
+ marginLeft:50,
+ marginTop:15,
+ 
+},
+background: {
+ 
+  resizeMode: 'cover', // or 'contain', 'stretch', 'repeat', 'center'
+  justifyContent: 'center',
+  width:300,
+  height:240,
+  marginTop:20,
+},
+image1:{
+ 
+  marginLeft:17,
+  marginBottom:110,
+ 
+},
+text5: {
+  fontSize: 15,
+  fontWeight: "bold",
+ 
+  marginLeft:10,
+  marginTop:10,
+ 
+ 
+},
+text6: {
+  fontSize: 15,
+  fontWeight: "bold",
+ 
+  marginLeft:10,
+ 
+ 
+ 
+},
+text7: {
+  fontSize: 13,
+  fontWeight: "bold",
+  marginLeft:-220,
+ 
+  marginTop:90,
+ 
+ 
+ 
+},
+image2:{
+ 
+  marginLeft:-235,
+ 
+ 
+},
+text8:{
+  marginTop:110,
+  marginLeft:-135,
+  width:182,
+  fontSize: 12,
+ 
+}
+ 
+ 
+})
